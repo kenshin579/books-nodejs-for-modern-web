a7013fdaa7d02adfac1c8fcdf3c52b1a92da6ce1
ykoh <kenshin579@gmail.com>
2018-04-13 22:39:42 +0900

    finish studying appendix B

--- Appendix/B/B-14.js
+++ Appendix/B/B-14.js
@@ -1,15 +1,19 @@
-var cluster = require('cluster')
+// 모듈을 추출합니다.
+var cluster = require('cluster');
 var os = require('os');
 
+// CPU COUNT를 출력합니다.
 var cpuCount = os.cpus().length;
-console.log('CPU count:', cpuCount);
+console.log('CPU Count:', cpuCount);
 
 if (cluster.isMaster) {
-    for(var i =0; i < cpuCount; i++) {
+    // 마스터 프로세서일 때
+    for (var i = 0; i < cpuCount; i++) {
         console.log('cluster.fork()');
         cluster.fork();
     }
+
 } else {
+    // 워커 프로세서일 때
     console.log('console.log()');
 }
-
