dd839a955a15fb35d571c2c9d4923b2036469a0e
ykoh <kenshin579@gmail.com>
2018-04-13 22:39:54 +0900

    finish studying appendix C

--- Appendix/C/C-1.js
+++ Appendix/C/C-1.js
@@ -12,7 +12,7 @@ for (var i = 0; i < 100; i++) {
 
 // 서버를 생성 및 실행합니다.
 http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.writeHead(200, {'Content-Type': 'application/json'});
     response.end(JSON.stringify(output));
 }).listen(52273, function () {
     console.log('server running at http://127.0.0.1:52273');
